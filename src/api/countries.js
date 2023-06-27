85b1a372a3efb1239ab10c92e2d4389d1dc363d7
Owusu Dennis Prah <padwusup@gmail.com>
2023-06-27 06:55:51 +0000

    Update api file to fetch details of a country

--- src/api/countries.js
+++ src/api/countries.js
@@ -1,5 +1,15 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
+export const countryLoader = async (cca3) => {
+  const res = await fetch(`${BASE_URL}/alpha/${cca3}`);
+
+  if (!res.ok) {
+    throw new Error('Something went wrong');
+  }
+
+  return res.json();
+};
+
 export const continentLoader = async (region) => {
   const res = await fetch(`${BASE_URL}/region/${region}`);
 
